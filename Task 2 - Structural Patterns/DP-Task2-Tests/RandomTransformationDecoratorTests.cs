b26c10d8710940a9b94468c079d1c2d0d007b359
AIvaylov02 <tonimen241@gmail.com>
2023-12-28 13:32:42 +0200

    Refactoring of RandomTransformation
    Covered it with unit tests and guaranteed that a new Transformation will be applied each time when invoked.

---
+++ Task 2 - Structural Patterns/DP-Task2-Tests/RandomTransformationDecoratorTests.cs
@@ -0,0 +1,76 @@
+﻿using DP_Task2.Interfaces;
+using DP_Task2.LabelDecorators;
+using DP_Task2.Labels;
+using DP_Task2.Transformations;
+
+namespace DP_Task2_Tests
+{
+    [TestFixture]
+    public class RandomTransformationDecoratorTests
+    {
+        // empty text
+        // no styles in ctor
+        // all styles applied in the end
+        const string SAMPLE_RICH_TEXT = "   September   Will Be   May     ";
+        const string EMPTY_TEXT = "";
+
+        [Test]
+        public void Test_RandomDecorator_Basic_Initialization_Empty_Text()
+        {
+            ILabel label = new SimpleLabel(EMPTY_TEXT);
+            RandomTransformationDecorator emptyDecorator = new RandomTransformationDecorator(label);
+            Assert.That(emptyDecorator.Text, Is.EqualTo(EMPTY_TEXT));
+        }
+
+        [Test]
+        public void Test_RandomDecorator_Basic_Initialization_Rich_Text()
+        {
+            ILabel label = new SimpleLabel(SAMPLE_RICH_TEXT);
+            RandomTransformationDecorator emptyDecorator = new RandomTransformationDecorator(label);
+            Assert.That(emptyDecorator.Text, Is.EqualTo(SAMPLE_RICH_TEXT));
+        }
+
+        [Test]
+        public void Test_RandomDecorator_Rich_Initialization_Rich_Text()
+        {
+            ILabel label = new SimpleLabel(EMPTY_TEXT);
+            List<ITextTransformation> transformations = new List<ITextTransformation>
+            {
+                new TrimLeftTransformation(),
+                new TrimRightTransformation(),
+                new SpaceNormalizationTransformation()
+            };
+            RandomTransformationDecorator fullEnrollmentDecorator = new RandomTransformationDecorator(label, transformations);
+            string currentText = fullEnrollmentDecorator.Text;
+            currentText = fullEnrollmentDecorator.Text;
+            currentText = fullEnrollmentDecorator.Text;
+            Assert.That(currentText, Is.EqualTo(EMPTY_TEXT));
+        }
+
+        [Test]
+        public void Test_RandomDecorator_Rich_Initialization_Empty_Text()
+        {
+            ILabel label = new SimpleLabel(SAMPLE_RICH_TEXT);
+            List<ITextTransformation> transformations = new List<ITextTransformation>
+            {
+                new TrimLeftTransformation(),
+                new TrimRightTransformation(),
+                new SpaceNormalizationTransformation()
+            };
+            RandomTransformationDecorator fullEnrollmentDecorator = new RandomTransformationDecorator(label, transformations);
+            string currentText = fullEnrollmentDecorator.Text;
+            currentText = fullEnrollmentDecorator.Text;
+            currentText = fullEnrollmentDecorator.Text;
+            const string EXPECTED_RESULT = "September Will Be May";
+            Assert.That(currentText, Is.EqualTo(EXPECTED_RESULT));
+            /* a full cycle has been applied, the transformations will be rerolled and are ready for round 2 of application
+             Although no more changes will be met as it is already an end product
+             */
+            currentText = fullEnrollmentDecorator.Text;
+            currentText = fullEnrollmentDecorator.Text;
+            currentText = fullEnrollmentDecorator.Text;
+            Assert.That(currentText, Is.EqualTo(EXPECTED_RESULT));
+        }
+        
+    }
+}
--- Task 2 - Structural Patterns/DP-Task2/LabelDecorators/RandomTransformationDecorator.cs
+++ Task 2 - Structural Patterns/DP-Task2/LabelDecorators/RandomTransformationDecorator.cs
@@ -5,32 +5,42 @@ namespace DP_Task2.LabelDecorators
     public class RandomTransformationDecorator : MultipleTransformationLabelDecorator
     {
         private Random generator;
-        public RandomTransformationDecorator(ILabel label) : base(label) 
+        private List<ITextTransformation> alreadyApplied;
+        public RandomTransformationDecorator(ILabel label) : base(label)
         {
             generator = new Random();
+            alreadyApplied = new List<ITextTransformation>();
         }
 
-        public RandomTransformationDecorator(ILabel label, List<ITextTransformation> transformationToClone) : base(label, transformationToClone) 
+        public RandomTransformationDecorator(ILabel label, List<ITextTransformation> transformationToClone) : base(label, transformationToClone)
         {
             generator = new Random();
+            alreadyApplied = new List<ITextTransformation>();
         }
 
         public override string Text
         {
             get
             {
-                if (transformations.Count == 0) // no valid transformations to apply
+                if (transformations.Count == 0 && alreadyApplied.Count == 0) // no valid transformations to apply
                 {
                     return label.Text;
                 }
                 else
                 {
+                    if (transformations.Count == 0) // all the transformations have already been applied, reroll them back
+                    {
+                        RerollTransformations();
+                    }
+
                     // the idea is that the final result state will be unchanged, even after removing a style
                     if (decoratedLabelContent is null) // the first transformation is yet to be applied
                         decoratedLabelContent = label.Text;
 
                     // since we have a valid list of transformations we can generate and apply a random transformation from it
-                    decoratedLabelContent = transformations[GenerateRandomValidNumber()].Transform(decoratedLabelContent);
+                    ITextTransformation randomTransformation = transformations[GenerateRandomValidNumber()];
+                    ApplyTransformation(randomTransformation);
+
                     // we want to keep the state after style removal, so we should store the string as an intermediary result
                     return decoratedLabelContent;
                 }
@@ -42,6 +52,25 @@ namespace DP_Task2.LabelDecorators
         {
             int generatedNum = generator.Next();
             return generatedNum % transformations.Count;
+        }
+
+        private void ApplyTransformation(ITextTransformation randomTransformation)
+        {
+            decoratedLabelContent = randomTransformation.Transform(decoratedLabelContent);
+            alreadyApplied.Add(randomTransformation);
+            transformations.Remove(randomTransformation);
+        }
+
+        private void RerollTransformations()
+        {
+            // for instead of foreach will grant us faster removal
+            ITextTransformation style;
+            for (int i = 0; i < alreadyApplied.Count; i++)
+            {
+                style = alreadyApplied[i];
+                alreadyApplied.RemoveAt(i);
+                transformations.Add(style);
+            }
         }
     }
 }
